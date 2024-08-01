208ed94548db894f646234dd7d8470c9857919ca
Clayton Priestley <clayton.priestley@gmail.com>
2024-07-31 19:34:31 -0500

    add mutations and queries to transaction resolver

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -1,6 +1,59 @@
+import Transaction from "../models/transaction.model.js";
 const transactionResolver = {
-  Query: {},
-  Mutation: {},
+  Query: {
+    transactions: async (_, __, context) => {
+      try {
+        if (!context.getUser()) throw new Error("Unauthorized");
+        const userId = await context.getUser()._id;
+
+        const transactions = await Transaction.find({ userId });
+        return transactions;
+      } catch (err) {
+        console.error("Error getting transactions: ", err);
+        throw new Error("Error getting transactions");
+      }
+    },
+    transaction: async (_, { transactionId }) => {
+      try {
+        const transaction = await Transaction.findById({ transactionId });
+        return transaction;
+      } catch (err) {
+        console.error("Error getting transaction: ", err);
+        throw new Error("Error getting transaction");
+      }
+    },
+    // TODO => IMPLEMENT categoryStatistics query
+    //categoryStatistics: async (_, __, context) => {},
+  },
+  Mutation: {
+    createTransaction: async (_, { input }, context) => {
+      try {
+        const newTransaction = new Transaction({
+          ...input,
+          userId: context.getUser()._id,
+        });
+        await newTransaction.save();
+        return newTransaction;
+      } catch (err) {
+        console.error("Error creating transaction: ", err);
+        throw new Error("Error creating transaction");
+      }
+    },
+    updateTransaction: async (_, { input }) => {
+      try {
+        const newTransaction = new Transaction({
+          ...input,
+          userId: context.getUser()._id,
+        });
+        await newTransaction.save();
+        return newTransaction;
+      } catch (err) {
+        console.error("Error updating transaction: ", err);
+        throw new Error("Error updating transaction");
+      }
+    },
+    deleteTransaction: async (_, { transactionId }, context) => {},
+  },
 };
 
 export default transactionResolver;
